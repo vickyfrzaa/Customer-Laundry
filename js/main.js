23dc1667cb9563e81ebdf293d8b4775b840fedf7
vickyfrzaa <vickyfahreza23@gmail.com>
2023-03-28 19:45:19 +0700

    lanjutan commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
-            $('#imagePreview').hide();
-            $('#imagePreview').fadeIn(650);
+$(document).ready(function () {
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+                $('#imagePreview').hide();
+                $('#imagePreview').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
         }
-        reader.readAsDataURL(input.files[0]);
     }
-}
-$("#imageUpload").change(function() {
-    readURL(this);
+    $("#imageUpload").change(function () {
+        readURL(this);
+    });
+
+    $("body").on("click", "input[name='product']", function () {
+        if ($("input[name='product']:checked").val() == 'other') {
+            $("section[name='product_other']").css('display', 'block');
+        } else {
+            $("section[name='product_other']").css('display', 'none');
+            $("section[name='product_other']").val('');
+        }
+    });
+
+    // $("body").on("click", "input[name='when']", function () {
+    //     if ($("input[name='when']:checked").val() == 'other') {
+    //         $("textarea[name='when_other']").css('display', 'block');
+    //     } else {
+    //         $("textarea[name='when_other']").css('display', 'none');
+    //         $("textarea[name='when_other']").val('');
+    //     }
+    // });
+
 });
